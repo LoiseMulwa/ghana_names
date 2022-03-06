53b2a5117fc2230a99da3b07a2c8ea9cf6e67f3a
LoiseMulwa <loisemulwa7@gmail.com>
2022-03-07 02:14:25 +0300

    ft: Added a math formula to calculate the day

--- js/script.js
+++ js/script.js
@@ -17,5 +17,46 @@ let days=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"
 var namesFemale=["Akosua","Abenaa","Akua","Yaa","Afua","Ama"];
 var namesMale=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kwame"];
 
+function calculate(){
+    let DD= parseInt(document.getElementById("born").value);
+    let MM = parseInt(document.getElementById("month").value);
+    let year = document.getElementById("year").value;
+
+    let cut=year.split('');
+    
+    c1 =cut[0];
+    c2=cut[1];
+    CC = parseInt(c1+c2);
+    // alert(CC);
+    y1=cut[2];
+    y2=cut[3]
+    YY = parseInt(y1+y2);
+    // alert(YY);
+    
+    let d = Math.floor( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7;
+    
+    alert(d);
+     }
+
+if (d==1 && gender=="male"){
+    let i=0;
+    for(i; i<=6; i++){
+        
+    }
+}
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
 
 
