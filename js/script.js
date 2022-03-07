d04356048ac7a4971ac8f129227fe2da0a7b096f
LoiseMulwa <loisemulwa7@gmail.com>
2022-03-07 15:23:50 +0300

    pt: Added the control flow to alert the akan names

--- js/script.js
+++ js/script.js
@@ -1,26 +1,37 @@
-let validate=document.getElementById("validate");
-let born=document.forms["validate"]["born"];
-let month=document.forms["validate"]["month"];
-let year=document.forms["validate"]["year"];
 
 
-function myLogin(){
-    if(born.value.length=="" || month.value.length=="" || year.value.length==""){
-        document.getElementById("validate").innerHTML="PLEASE FILL OUT ALL FIELDS!!"
-        // alert("Please fill out all fields");
-        return false;
-    }
-}
+// function myLogin(){
+//     if(born.value.length=="" || month.value.length=="" || year.value.length==""){
+//         document.getElementById("validate").innerHTML="PLEASE FILL OUT ALL FIELDS!!"
+//         // alert("Please fill out all fields");
+//         return false;
+//     }
+// }
 
-let days=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
-var namesFemale=["Akosua","Abenaa","Akua","Yaa","Afua","Ama"];
-var namesMale=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kwame"];
+
 
 function calculate(){
+// let validate=document.getElementById("validate");
+// let born=document.forms["validate"]["born"];
+// let month=document.forms["validate"]["month"];
+// var year=document.forms["validate"]["year"];
+
+// if(born.value.length=="" || month.value.length=="" || year.value.length==""){
+//     document.getElementById("validate").innerHTML="PLEASE FILL OUT ALL FIELDS!!"
+//     // alert("Please fill out all fields");
+//     return false;
+// }
+
+
+let days=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+var female=["Akosua","Abenaa","Akua","Yaa","Afua","Ama"];
+var male=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kwame"];
     let DD= parseInt(document.getElementById("born").value);
     let MM = parseInt(document.getElementById("month").value);
     let year = document.getElementById("year").value;
-    let date=document.getElementById("validate").value;
+    // let date=document.getElementById("validate").value;
+
+
 
     let cut=year.split('');
     
@@ -34,15 +45,35 @@ function calculate(){
     YY = parseInt(y1+y2);
     // alert(YY);
     
-    let d = (Math.floor(CC / 4) - 2 * CC - 1 + 5 * Math.floor(YY / 4) + Math.floor((26 * (MM + 1)) / 10) + DD) % 7;
+    let d = Math.floor(( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7 );
+    // alert(d);
+    // man=document.getElementById("male").value;
+    // alert(male[d]);
+
     // ourGender(d, date)
     
-    alert(d);
-     }
+    // alert(d);
 
-let gender =document.getElementById("gender").value;
-     alert(gender.value)
-     alert(d)
+    var gender = document.getElementById("gender").value;
+    // console.log(gender);
+    if (gender === "male"){
+    sex=male;
+    }
+    else{
+    sex=female;
+    }
+    alert(sex[d]);
+};
+     
+
+
+    //  alert(gender)
+    //  alert(d)
+     
+
+// document.getElementById("gender").innerHTML =gender;
+    
+    
 
 // function ourGender(day, date){ 
 // let gender = document.getElementById("gender").value;
@@ -65,27 +96,5 @@ let gender =document.getElementById("gender").value;
 //          // document.getElementById("row").display="none";
  
 //      }
-//     }
-
-
-      
-
-
-
-
-
-
-
-
-
-
-    
-
-
-
-
-
-
-
 
 
