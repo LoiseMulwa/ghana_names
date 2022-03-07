78115ac72faa1f7c90629458b8d7cd71b1459836
LoiseMulwa <loisemulwa7@gmail.com>
2022-03-07 12:36:07 +0300

    pt: declared a new var gender

--- js/script.js
+++ js/script.js
@@ -3,13 +3,12 @@ let born=document.forms["validate"]["born"];
 let month=document.forms["validate"]["month"];
 let year=document.forms["validate"]["year"];
 
+
 function myLogin(){
     if(born.value.length=="" || month.value.length=="" || year.value.length==""){
         document.getElementById("validate").innerHTML="PLEASE FILL OUT ALL FIELDS!!"
         // alert("Please fill out all fields");
         return false;
-
-
     }
 }
 
@@ -21,34 +20,59 @@ function calculate(){
     let DD= parseInt(document.getElementById("born").value);
     let MM = parseInt(document.getElementById("month").value);
     let year = document.getElementById("year").value;
+    let date=document.getElementById("validate").value;
 
     let cut=year.split('');
     
     c1 =cut[0];
     c2=cut[1];
     CC = parseInt(c1+c2);
+
     // alert(CC);
     y1=cut[2];
     y2=cut[3]
     YY = parseInt(y1+y2);
     // alert(YY);
     
-    let d = Math.floor( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7;
+    let d = (Math.floor(CC / 4) - 2 * CC - 1 + 5 * Math.floor(YY / 4) + Math.floor((26 * (MM + 1)) / 10) + DD) % 7;
+    // ourGender(d, date)
     
     alert(d);
      }
 
-     document.getElementById("gender");
+let gender =document.getElementById("gender").value;
+     alert(gender.value)
+     alert(d)
 
-if (gender=="male"){
-    let i=0;
-    for(i; i<=6; i++){
-        if(i==Math.floor(d));
-        alert("Your akan name is"+namesMale[i])
+// function ourGender(day, date){ 
+// let gender = document.getElementById("gender").value;
+// let akanName
+// let day
+//     //  alert(gender.value);
+//     //  alert(d);
+     
+//      if(gender=="male"){
+//          akanName=namesMale[day]
+//          day=days[day]
+        
+//          alert("Your Akan name is " + akanName);
+//          // document.getElementById("row").display="none";
+//      }
+//      else if(gender =="female"){
+//         akanName=namesFemale[day]
+//         day=days[day]
+//          alert("Your Akan Name is " + akanName);
+//          // document.getElementById("row").display="none";
+ 
+//      }
+//     }
 
 
-    }
-}
+      
+
+
+
+
 
 
 
